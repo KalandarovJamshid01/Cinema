f8e7a8fc44ba87b669ac6a20b65ddd40b564247c
Jamshid <kalandarovjamshid01@gmail.com>
2022-04-18 12:27:22 +0500

    html css is proccessing

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+export const API_URL = `https://www.omdbapi.com/?apikey=1fd18c03&s`;
+export const TIMEOUT_SEC = 5;
+export const PAGINATION_NUM = 10;
---
+++ src/js/model.js
@@ -0,0 +1,7 @@
+export const state = {};
+export const loadCinema = async function () {
+  try {
+  } catch (error) {
+    alert(err);
+  }
+};
