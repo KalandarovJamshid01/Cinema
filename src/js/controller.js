e5cca69b9279c44eef8c32661ae6f32cfb09297f
Jamshid <kalandarovjamshid01@gmail.com>
2022-04-18 10:19:04 +0500

    get api

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const { async } = require("regenerator-runtime");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -7,7 +7,16 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+const api = async function () {
+  const cinema = await fetch("https://www.omdbapi.com/?apikey=1fd18c03&s=moon");
+  console.log(cinema);
+  const body = await cinema.json();
+  console.log(body);
+  let html = `<div><img src="${body.Search[0].Poster}" alt=""></div>`;
+  document.querySelector("body").insertAdjacentHTML("afterbegin", html);
+};
 
+api();
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
